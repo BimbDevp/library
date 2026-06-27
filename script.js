f1fe681f2943dd0f2ef99b1cf96037d46563767a
BimbDevp <BimbDevp@users.noreply.github.com>
2026-06-27 13:20:23 +0700

    feat: add query selector

---
+++ script.js
@@ -0,0 +1,14 @@
+// Query selector
+const search = document.querySelector("input");
+const find = document.querySelector(".submit-btn");
+const add = document.querySelector(".add-btn");
+
+// Logic
+
+
+
+// Display
+
+
+
+// Event
