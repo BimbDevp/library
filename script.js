964baba7f0b453f49ee1f01a32c16ba2b3593924
BimbDevp <BimbDevp@users.noreply.github.com>
2026-06-28 16:26:35 +0700

    feat: add logic to add book and find book button

--- script.js
+++ script.js
@@ -5,25 +5,20 @@ const search = document.querySelector("input");
 const find = document.querySelector(".submit-btn");
 const add = document.querySelector(".add-btn");
 const display = document.querySelector(".book-container");
+const bookDetails = document.querySelector(".book-details");
+const addNewBook = document.querySelector(".add-container");
+const submitBook = document.querySelector(".submit-book");
+const closeDialog = document.querySelector(".close-dialog");
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const statusBook = document.querySelector("input[name='status']:checked");
+const bookForm = document.querySelector(".book-form")
 
 // Logic
 
 // storage
-const myLibrary = [
-    {   id: 123456,
-        title: "Berani Tidak disukai",
-        author: "Ichiro Kishimi & Fumitake Koga",
-        pages: 336,
-        status: "unread",
-    },
-    {
-        id: 123457,
-        title: "Laut Bercerita",
-        author: "Laila S. Chudori",
-        pages: 394,
-        status: "unread",
-    }
-];
+const myLibrary = [];
 
 // constructor
 function Book(title, author, pages, status) {
@@ -35,11 +30,52 @@ function Book(title, author, pages, status) {
 }
 
 function addBookToLibrary(title, author, pages, status) {
+    
     const newBook = new Book(title, author, pages, status);
     myLibrary.push(newBook);
 }
 
+function searchBookInLibrary(title) {
+    bookDetails.innerHTML = "";
+    const foundBook = myLibrary.find(book => book.title === title)
+        if (foundBook) {
+            const id = document.createElement("p");
+            id.textContent = `ID: ${foundBook.id}`;
+
+            const title = document.createElement("p");
+            title.textContent = `Title: ${foundBook.title}`;
+
+            const author = document.createElement("p");
+            author.textContent = `Author: ${foundBook.author}`;
+
+            const pages = document.createElement("p");
+            pages.textContent = `Pages: ${foundBook.pages}`;
+
+            const status = document.createElement("p");
+            status.textContent = `Status: ${foundBook.status}`;
+
+
+            bookDetails.appendChild(id);
+            bookDetails.appendChild(title);
+            bookDetails.appendChild(author);
+            bookDetails.appendChild(pages);
+            bookDetails.appendChild(status);
+        
+            bookDetails.showModal();
+        } else {
+            const information = document.createElement("h1");
+            information.textContent = "Book not found!";
+
+
+            bookDetails.appendChild(information);
+            bookDetails.showModal();
+        }
+ 
+}
+
 function displayBook(arr){
+    display.innerHTML = "";
+
     arr.forEach(book => {
         const div = document.createElement("div");
         div.classList.add("card");
@@ -71,7 +107,43 @@ function displayBook(arr){
 }
 
 
-// Display
-displayBook(myLibrary);
+
+
 
 // Event
+
+
+
+find.addEventListener("click", (e) => {
+    e.preventDefault();
+    searchBookInLibrary(search.value); 
+})
+
+add.addEventListener("click", () => addNewBook.showModal());
+
+submitBook.addEventListener("click", (e) => {
+    e.preventDefault();
+    statusBook;
+
+    const getStatus = statusBook ? statusBook.value : "unread";
+
+    addBookToLibrary(title.value, author.value, pages.value, getStatus);
+
+    displayBook(myLibrary);
+
+    addNewBook.close();
+
+    bookForm.reset();
+})
+
+closeDialog.addEventListener("click",() => addNewBook.close());
+
+
+
+
+
+
+
+addBookToLibrary("Berani Tidak Disukai", "Ichiro Kishimi & Fumitake Koga", 336, "unread");
+addBookToLibrary("Laut Bercerita", "Laila S. Chudori", 394, "unread");
+displayBook(myLibrary);
